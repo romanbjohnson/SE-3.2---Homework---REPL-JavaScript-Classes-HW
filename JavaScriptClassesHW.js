8d62c4c18215fd2d572584b5409c0a5ab0b21aa7
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-03-03 23:05:38 -0500

    Add files via upload

---
+++ JavaScriptClassesHW.js
@@ -0,0 +1,76 @@
+//Roman Johnson
+
+
+
+class Cat {
+
+  //at least three properties are here below
+const rihanna = {
+  a: 1,
+  b() {},
+};
+
+  const james = {
+  c: 1,
+  d() {},
+};
+}
+console.log(Cat)
+
+
+class Pirate {
+
+  constructor(){
+
+  }
+
+}
+
+// 3 methods 
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 5566,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+const person2 = {
+  firstName: "Terry",
+  lastName: "Crews",
+  id: 5567,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+
+const person3 = {
+  firstName: "Oprah",
+  lastName: "Winfrey",
+  id: 5568,
+  fullName: function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+// Second cat instance 
+
+
+class Cat {
+
+  //at least three properties are here below
+const bobby = {
+  a: 5,
+  b() {},
+};
+
+  const james = {
+  c: 8,
+  d() {},
+};
+}
+
+console.log(Cat)
